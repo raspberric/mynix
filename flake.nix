d55f2ac5d14ef5c8deb9978b6bc52aa372acca09
Raspberric <nikolamalinovic42@gmail.com>
2025-08-29 18:48:38 +0200

    Initialize config

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    mvim.url = "path:./mvim";
+  };
+  outputs = {self, nixpkgs, mvim}: 
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      packages.${system}.default = pkgs.symlinkJoin {
+        name = "mySystem";
+        paths = with pkgs; [
+	  tmux
+	  lazygit
+	  mvim.packages.${system}.default
+	];
+
+      };
+    };
+}
