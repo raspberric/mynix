82716a1af3b6d9d04f940f20ef0619f030422a4b
Raspberric <nikolamalinovic42@gmail.com>
2025-09-03 03:39:30 +0200

    Configure snacks and keymaps

--- mvim/nvim/init.lua
+++ mvim/nvim/init.lua
@@ -1,20 +1,11 @@
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
+vim.g.mapleader = ' '
 
-require('mini_config').setup()
 require('which-key').setup()
+require('mini_config').setup()
+require('snacks_config').setup()
 
-require('snacks').setup({
-	opts = {
-		explorer = {
-			enabled = true,
-			replace_netrw = true,
-		},
-		notifier = {
-			enabled = true
-		}, 
-	}
-})
-vim.keymap.set('n', '<leader>e', Snacks.explorer.reveal, { desc = 'Reveal current file in explorer' })
 
 -- local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
