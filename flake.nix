ce8209e9d29ea3f949301a1bdc3efbf353a16e9a
Raspberric <nikolamalinovic42@gmail.com>
2025-09-05 19:35:17 +0200

    Setup alacritty

--- flake.nix
+++ flake.nix
@@ -3,15 +3,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     mvim.url = "path:./mvim";
+    nixgl.url = "github:nix-community/nixGL";
   };
   outputs = {
     self,
     nixpkgs,
     mvim,
+    nixgl,
   }: let
     system = "x86_64-linux";
+    wrapWithNixGL = final: prev: {
+      alacritty = final.writeShellScriptBin "alacritty" ''
+        exec ${nixgl.packages.x86_64-linux.nixGLDefault}/bin/nixGL ${prev.alacritty}/bin/alacritty "$@"
+      '';
+    };
     pkgs = import nixpkgs {
       inherit system;
+      overlays = [wrapWithNixGL];
     };
   in {
     packages.${system}.default = pkgs.symlinkJoin {
@@ -21,6 +29,8 @@
         tmux
         lazygit
         mvim.packages.${system}.default
+        nixgl
+        alacritty
         nodejs_24
       ];
     };
