90cb3c6a027cc35f56d7b23d92996e047ac1fe1f
Raspberric wsl <nikolamalinovic42@gmail.com>
2025-11-14 18:44:47 +0000

    Tweak blink-cmp

--- software/mvim/nvim/lua/blink.lua
+++ software/mvim/nvim/lua/blink.lua
@@ -3,10 +3,12 @@ local M = {}
 function M.setup()
   require("blink.cmp").setup({
     sources = {
-      default = { "lsp", "path", "snippets" },
+      default = { "lsp", "path", "lazydev", "snippets", "buffer" },
       providers = {
-        lsp = {
-          score_offset = 1,
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
         },
       },
     },
@@ -19,12 +21,13 @@ function M.setup()
       ["<C-y>"] = { "accept", "fallback" },
 
       ["<C-j>"] = { "select_next" },
-      ["<C-k>"] = { "select_prev" },
+      ["<C-k>"] = { "select_prev", "show_signature", "hide_signature" },
 
       ["<C-h>"] = { "scroll_documentation_up" },
       ["<C-l>"] = { "scroll_documentation_down" },
     },
     cmdline = {
+      enabled = true,
       keymap = {
         preset = "inherit",
       },
@@ -34,28 +37,7 @@ function M.setup()
       ghost_text = { enabled = true },
       menu = {
         draw = {
-          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", "source_name" } },
-          -- components = {
-          -- kind_icon = {
-          --   text = function(ctx)
-          --     local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-          --     return kind_icon
-          --   end,
-          --   -- (optional) use highlights from mini.icons
-          --   highlight = function(ctx)
-          --     local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-          --     return hl
-          --   end,
-          -- },
-          -- kind = {
-          --
-          --   -- (optional) use highlights from mini.icons
-          --   highlight = function(ctx)
-          --     local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-          --     return hl
-          --   end,
-          -- },
-          -- },
+          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", "source_name", gap = 1 } },
         },
       },
     },
