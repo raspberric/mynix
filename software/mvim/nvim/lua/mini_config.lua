858a793b0267a6358cabfaeadadde4e656f67255
Raspberric wsl <nikolamalinovic42@gmail.com>
2025-11-14 06:30:22 +0000

    Tweak mini config and enable clue
    Removes the mini.deps installation since it's managed through nix

--- software/mvim/nvim/lua/mini_config.lua
+++ software/mvim/nvim/lua/mini_config.lua
@@ -1,26 +1,5 @@
 local M = {}
 
--- Installs mini deps manager if not there
-function check_and_install()
-  local path_package = vim.fn.stdpath("data") .. "/site"
-  local mini_path = path_package .. "/pack/deps/start/mini.nvim"
-  if not vim.loop.fs_stat(mini_path) then
-    vim.cmd('echo "Installing `mini.nvim`" | redraw')
-    local clone_cmd = {
-      "git",
-      "clone",
-      "--filter=blob:none",
-      -- Uncomment next line to use 'stable' branch
-      -- '--branch', 'stable',
-      "https://github.com/nvim-mini/mini.nvim",
-      mini_path,
-    }
-    vim.fn.system(clone_cmd)
-    vim.cmd("packadd mini.nvim | helptags ALL")
-    vim.cmd('echo "Installed `mini.nvim`" | redraw')
-  end
-end
-
 function M.setup()
   require("mini.deps").setup({ path = { package = path_package } })
   -- local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
@@ -31,12 +10,8 @@ function M.setup()
       move_with_alt = true,
     },
   })
+  require("mini.pairs").setup()
   require("mini.move").setup()
-  require("mini.jump").setup({
-    delay = {
-      idle_stop = 2000,
-    },
-  })
   require("mini.indentscope").setup()
   require("mini.notify").setup()
   require("mini.git").setup()
@@ -62,8 +37,61 @@ function M.setup()
   require("mini.tabline").setup({
     show_icons = true,
   })
-  require("mini.surround").setup()
-  require("mini.clue").setup()
+  require("mini.surround").setup({
+    add = "<leader>sa",
+    delete = "<leader>sd",
+    replace = "<leader>sr",
+  })
+  local miniclue = require("mini.clue")
+  miniclue.setup({
+    window = { delay = 0, config = { width = 60 } },
+    triggers = {
+      -- Leader triggers
+      { mode = "n", keys = "<Leader>" },
+      { mode = "x", keys = "<Leader>" },
+
+      -- `[` and `]` keys
+      { mode = "n", keys = "[" },
+      { mode = "n", keys = "]" },
+
+      -- Built-in completion
+      { mode = "i", keys = "<C-x>" },
+
+      -- `g` key
+      { mode = "n", keys = "g" },
+      { mode = "x", keys = "g" },
+
+      -- Marks
+      { mode = "n", keys = "'" },
+      { mode = "n", keys = "`" },
+      { mode = "x", keys = "'" },
+      { mode = "x", keys = "`" },
+
+      -- Registers
+      { mode = "n", keys = '"' },
+      { mode = "x", keys = '"' },
+      { mode = "i", keys = "<C-r>" },
+      { mode = "c", keys = "<C-r>" },
+
+      -- Window commands
+      { mode = "n", keys = "<C-w>" },
+
+      -- `z` key
+      { mode = "n", keys = "z" },
+      { mode = "x", keys = "z" },
+    },
+
+    clues = {
+      -- Enhance this by adding descriptions for <Leader> mapping groups
+      miniclue.gen_clues.square_brackets(),
+      miniclue.gen_clues.builtin_completion(),
+      miniclue.gen_clues.g(),
+      miniclue.gen_clues.marks(),
+      miniclue.gen_clues.registers({ show_contents = true }),
+      miniclue.gen_clues.windows(),
+      miniclue.gen_clues.z(),
+    },
+  })
 end
 
 return M
