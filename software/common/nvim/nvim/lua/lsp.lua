c99e8c1a11a46e10b7a6d17269285150be030aca
Raspberric <nikolamalinovic42@gmail.com>
2025-12-15 07:01:56 +0100

    Configure angular lsp

--- software/common/nvim/nvim/lua/lsp.lua
+++ software/common/nvim/nvim/lua/lsp.lua
@@ -5,10 +5,44 @@ return {
     vim.lsp.config("nixd", { capabilities = lspCapabilities })
     vim.lsp.config("ts_ls", { capabilities = lspCapabilities })
     vim.lsp.config("jsonls", { capabilities = lspCapabilities })
-    vim.lsp.config("html", { capabilities = lspCapabilities })
+    vim.lsp.config("html", {
+      filetypes = { "html" },
+      root_dir = function(bufnr)
+        -- do not attach inside Angular projects
+        local root = vim.fs.root(bufnr, { "angular.json" })
+        return root == nil and vim.fs.root(bufnr, { "package.json" })
+      end,
+      capabilities = lspCapabilities,
+    })
     vim.lsp.config("cssls", { capabilities = lspCapabilities })
     vim.lsp.config("tailwindcss", { capabilities = lspCapabilities })
-    vim.lsp.config("angularls", { capabilities = lspCapabilities })
+    vim.lsp.config("angularls", {
+      cmd = {
+        "ngserver",
+        "--stdio",
+        "--tsProbeLocations",
+        "",
+        "--ngProbeLocations",
+        "",
+      },
+      before_init = function(params, config)
+        local root = config.root_dir
+        config.cmd = {
+          "ngserver",
+          "--stdio",
+          "--tsProbeLocations",
+          root .. "/node_modules",
+          "--ngProbeLocations",
+          root .. "/node_modules",
+        }
+      end,
+      filetypes = { "typescript", "html", "typescriptreact" },
+      root_dir = vim.fs.root(0, {
+        "angular.json",
+        "package.json",
+      }),
+      capabilities = lspCapabilities,
+    })
     vim.lsp.config("astro", {
       init_options = {
         typescript = {
