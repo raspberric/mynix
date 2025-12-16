7916a9ec07e880394c979064704c0213d130d770
Raspberric <nikolamalinovic42@gmail.com>
2025-12-16 06:50:57 +0100

    Split config based on nixCats flag

--- software/common/nvim/nvim/init.lua
+++ software/common/nvim/nvim/init.lua
@@ -3,27 +3,40 @@ vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 vim.g.netrw_liststyle = 3
+vim.opt.signcolumn = "yes"
+
+-- Fix for Lua LSP "undefined global" warning on vim.fn.nixCats
+-- This stub function is ignored by Neovim but recognized by the LSP
+vim.fn.nixCats = vim.fn.nixCats or function(path)
+  return {}
+end
 
 vim.cmd("colorscheme tokyonight-storm")
-require("blink").setup()
 require("mini_config").setup()
 require("snacks_config").setup()
 require("formatter").setup()
-require("lsp").setup()
 require("treesitter").setup()
 require("persistence_config").setup()
-require("ccc_config").setup()
-require("debug_config").setup()
 require("trouble_config").setup()
 require("flash_config").setup()
 require("refactor_config").setup()
 require("opencode").setup()
 
+-- Handle frontend development with coc.nvim or other LSP setup
+if nixCats("runtimeChecks.IS_FRONTEND") then
+  require("frontend.coc_config").setup()
+  require("frontend.ccc_config").setup()
+  require("debug_config").setup()
+else
+  require("blink").setup()
+  require("lsp").setup()
+end
+
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
 })
-vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Code Action" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
---
+++ software/common/nvim/nvim/lua/frontend/lsp.lua
@@ -0,0 +1,16 @@
+return {
+  setup = function()
+    -- vim.lsp.enable("lua_ls")
+    -- vim.lsp.enable("nixd")
+    -- vim.lsp.enable("ts_ls")
+    -- vim.lsp.enable("jsonls")
+    -- vim.lsp.enable("html")
+    -- vim.lsp.enable("cssls")
+    -- vim.lsp.enable("astro")
+    -- vim.lsp.enable("angularls")
+    -- vim.lsp.enable("tailwindcss")
+    --
+    -- require("nvim-ts-autotag").setup()
+    --
+  end,
+}
