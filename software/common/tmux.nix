db6f4237d70b3cc04bc334ffb04c0ce4def94c0e
Raspberric <nikolamalinovic42@gmail.com>
2026-01-14 17:36:24 +0100

    Add tmux keybinds for joining windows and tmux-resurrect

--- software/common/tmux.nix
+++ software/common/tmux.nix
@@ -5,9 +5,13 @@
     # copy tmux-yank contents to clipboard
     bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -i -sel clipboard"
     run-shell "tmux-yank"
+    run-shell "tmux-resurrect"
     # move windows left and right
     bind-key -n M-C-h swap-window -t -1\; select-window -t -1
     bind-key -n M-C-l swap-window -t +1\; select-window -t +1
+    # join windows
+    bind-key j choose-window -F "#{window_index}: #{window_name}" "join-pane -h -t %%"
+    bind-key J choose-window -F "#{window_index}: #{window_name}" "join-pane -v -t %%"
   '';
 in
   pkgs.writeShellApplication {
@@ -15,6 +19,7 @@ in
     runtimeInputs = [
       pkgs.tmux
       pkgs.tmuxPlugins.yank
+      pkgs.tmuxPlugins.resurrect
     ];
     text = ''
       exec ${pkgs.tmux}/bin/tmux -f "${tmuxConfigContent}" "$@"
