6bd8daadb4603a6530d7579f81660261fe822d35
Raspberric <nikolamalinovic42@gmail.com>
2026-01-24 04:38:57 +0100

    Make tmux-resurrect work
    And add prefix + C work to open a new window in the cwd

--- software/common/tmux.nix
+++ software/common/tmux.nix
@@ -1,18 +1,27 @@
 {pkgs, ...}: let
-  tmuxConfigContent = builtins.toFile "tmux.conf" ''
-    setw -g mode-keys vi
+  tmuxConfigContent = pkgs.writeTextFile {
+    name = "tmux.conf";
+    text = ''
+      setw -g mode-keys vi
 
-    # copy tmux-yank contents to clipboard
-    bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -i -sel clipboard"
-    run-shell "tmux-yank"
-    run-shell "tmux-resurrect"
-    # move windows left and right
-    bind-key -n M-C-h swap-window -t -1\; select-window -t -1
-    bind-key -n M-C-l swap-window -t +1\; select-window -t +1
-    # join windows
-    bind-key j choose-window -F "#{window_index}: #{window_name}" "join-pane -h -t %%"
-    bind-key J choose-window -F "#{window_index}: #{window_name}" "join-pane -v -t %%"
-  '';
+      # copy tmux-yank contents to clipboard
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -i -sel clipboard"
+      run-shell "tmux-yank"
+
+      run-shell ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/resurrect.tmux
+
+      # move windows left and right
+      bind-key -n M-C-h swap-window -t -1\; select-window -t -1
+      bind-key -n M-C-l swap-window -t +1\; select-window -t +1
+
+      # join windows
+      bind-key j choose-window -F "#{window_index}: #{window_name}" "join-pane -h -t %%"
+      bind-key J choose-window -F "#{window_index}: #{window_name}" "join-pane -v -t %%"
+
+      # open a new window in cwd
+      bind-key C new-window -c "#{pane_current_path}"
+    '';
+  };
 in
   pkgs.writeShellApplication {
     name = "tmux";
