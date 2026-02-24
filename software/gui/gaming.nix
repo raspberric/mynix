1eadb63363ffb598462a6519a050c5830bfe4d18
Raspberric <nikolamalinovic42@gmail.com>
2026-02-24 22:47:28 +0100

    Add ds-beamng utility

--- software/gui/gaming.nix
+++ software/gui/gaming.nix
@@ -1,5 +1,20 @@
-{...}: {
+{pkgs, ...}: 
+let
+  ds-beamng = pkgs.writeShellApplication {
+    name = "ds-beamng";
+    runtimeInputs = [pkgs.dualsensectl];
+    text = ''
+      dualsensectl trigger left feedback-raw 0 0 0 4 4 4 8 8 8 8
+      dualsensectl trigger right feedback-raw 2 2 2 3 3 3 4 4 5 5
+    '';
+  };
+in
+{
   programs = {
     steam.enable = true;
   };
+
+  environment.systemPackages = [
+    ds-beamng
+  ];
 }
