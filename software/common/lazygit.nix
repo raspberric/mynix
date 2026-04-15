1b925f2201d7ccfc1963066ba07d3ec66c288f46
Raspberric <nikolamalinovic42@gmail.com>
2026-04-15 18:05:41 +0200

    Configure lazygit to use difftastic

--- software/common/lazygit.nix
+++ software/common/lazygit.nix
@@ -2,12 +2,17 @@
   configFile = builtins.toFile "lazygit-config.yml" ''
     git:
       branchLogCmd: "git log --graph --all --color=always --abbrev-commit --decorate --date=relative --pretty=medium {{branchName}} --"
+      pagers:
+        - pager: ""
+        - externalDiffCommand: "difft --color=always"
+        - externalDiffCommand: "difft --color=always --display=inline"
   '';
 in
   pkgs.writeShellApplication {
     name = "lazygit";
     runtimeInputs = [
       pkgs.lazygit
+      pkgs.difftastic
     ];
     text = ''
       exec ${pkgs.lazygit}/bin/lazygit -ucf "${configFile}" "$@"
