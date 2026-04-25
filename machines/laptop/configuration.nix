929ac9e5aa856262cc263d3ed00f051bfe5dcc7d
Raspberric <nikolamalinovic42@gmail.com>
2026-04-25 14:33:00 +0200

    Clean up leftover hermes reference

--- machines/laptop/configuration.nix
+++ machines/laptop/configuration.nix
@@ -3,8 +3,6 @@
   pkgs,
   ...
 }: {
-  services.hermes-agent.enable = true;
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
