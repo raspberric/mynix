5110c1d0cde7031a7a6c136d9a368688f126781c
Raspberric <nikolamalinovic42@gmail.com>
2026-06-24 00:46:11 +0200

    Configure podman to use docker-compose

--- software/modules/podman.nix
+++ software/modules/podman.nix
@@ -2,5 +2,11 @@
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
+    dockerSocket.enable = true;
+    defaultNetwork.settings.dns_enabled = true; # ← add: app container resolves `db`, `minio`, `valkey-master`
   };
+  environment.systemPackages = with pkgs; [
+    podman-compose
+    docker-compose
+  ];
 }
