87d0d3a180b3760bd5f3bf838b0d54a7e690d4dd
Raspberric <nikolamalinovic42@gmail.com>
2026-07-16 00:07:07 +0200

    Configure nvim-hydra

--- software/common/nvim/nvim/lua/hydra_config.lua
+++ software/common/nvim/nvim/lua/hydra_config.lua
@@ -26,7 +26,7 @@ Window resize
 _+_: taller     _-_: shorter
 _>_: wider      _<_: narrower
 _=_: equal      _|_: max width
-___: max height _q_: quit
+_q_: quit       _: max height
 ]],
     heads = {
       { "+", command("resize +2"), { desc = "taller" } },
@@ -35,7 +35,35 @@ ___: max height _q_: quit
       { "<", command("vertical resize -4"), { desc = "narrower" } },
       { "=", command("wincmd ="), { desc = "equal" } },
       { "|", command("vertical resize 9999"), { desc = "max width" } },
-      { "_", command("resize 9999"), { desc = "max height" } },
+      { "_", command("resize 9999"), { desc = false } },
+      { "q", nil, { exit = true, desc = "quit" } },
+      { "<Esc>", nil, { exit = true, desc = false } },
+    },
+  })
+
+  Hydra({
+    name = "Side scroll",
+    mode = "n",
+    body = "z",
+    config = {
+      color = "red",
+      hint = {
+        type = "window",
+        position = "bottom",
+        border = "rounded",
+      },
+    },
+    hint = [[
+Side scroll
+_h_: left 5 cols        _l_: right 5 cols
+_H_: left half-screen   _L_: right half-screen
+_q_: quit
+]],
+    heads = {
+      { "h", "5zh", { desc = "left 5 cols" } },
+      { "l", "5zl", { desc = "right 5 cols" } },
+      { "H", "zH", { desc = "left half-screen" } },
+      { "L", "zL", { desc = "right half-screen" } },
       { "q", nil, { exit = true, desc = "quit" } },
       { "<Esc>", nil, { exit = true, desc = false } },
     },
